209e269ef0322dfa91e50247a49d3520c7061b52
Walter-PC\Walter <walterbhmg@hotmail.com>
2017-05-05 00:20:28 -0300

    Código Fonte 04/05

---
+++ DilmaRevenge/Assets/script/Music.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Music : MonoBehaviour {
+
+	public Texture2D musicOn; 
+	public Texture2D musicOff; 
+	public bool ativo;
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		AudioSource a = GetComponent<AudioSource> ();
+		GUITexture g = GetComponent<GUITexture>();
+
+		if(ativo) {
+			a.mute = false;
+			g.texture = musicOn;
+		} else {
+			a.mute = true;
+			g.texture = musicOff;
+		}	
+	}
+	void OnMouseDown() {
+		ativo = !ativo;
+	}
+}
---
+++ DilmaRevenge/Assets/script/Player.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	public float velocidade;
+	public float forcaPulo;
+	private bool estaNoChao;
+	public Transform chaoVerificador;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		Movimentacao();
+	}
+
+	void Movimentacao() {
+
+		estaNoChao = Physics2D.Linecast(transform.position, chaoVerificador.position, 1 << LayerMask.NameToLayer("Piso"));
+		if (Input.GetAxisRaw("Horizontal") > 0 ) {
+			transform.Translate (Vector2.right * velocidade * Time.deltaTime);
+			transform.eulerAngles = new Vector2(0, 0);
+		}
+
+		if (Input.GetAxisRaw("Horizontal") < 0 ) {
+			transform.Translate (Vector2.right * velocidade * Time.deltaTime);
+			transform.eulerAngles = new Vector2(0, 180);
+		}
+
+
+		Rigidbody2D rb = GetComponent<Rigidbody2D>();
+		if (Input.GetButtonDown("Jump") && estaNoChao) {
+			rb.AddForce(transform.up * forcaPulo);
+		}
+	}
+}
