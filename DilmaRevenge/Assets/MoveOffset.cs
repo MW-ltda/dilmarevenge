9014eb7b42368e68b55025c5b9777f0bbf5569cc
CamilaScaldaferri <camilascaldaferri@gmail.com>
2017-05-25 16:38:17 -0300

    Código Fonte 25/05

---
+++ DilmaRevenge/Assets/MoveOffset.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveOffset : MonoBehaviour {
+
+	private Material currentMaterial;
+	private float speed;
+	private float offSet;
+
+
+	// Use this for initialization
+	void Start () {
+		currentMaterial = GetComponent<Renderer>().material;
+
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		offSet += 0.001f;
+
+		currentMaterial.SetTextureOffset("_MainTex", new Vector2(offSet,0));
+
+	}
+}
